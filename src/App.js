32ec7ab6b983b05a695cdb307f7261de0134a71f
aris winandi <winandiaris@gmail.com>
2019-10-28 16:45:58 +0700

    membuat header dan link home, shop page, dan contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 //import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <HomePage></HomePage> */}
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
