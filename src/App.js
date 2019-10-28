1994054fa72a7f292d681e3eded3f93fe4f3ff81
aris winandi <winandiaris@gmail.com>
2019-10-28 12:23:29 +0700

    update menu-item menggunakan withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 //import logo from './logo.svg';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>hat page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
+      
     </div>
   );
 }
