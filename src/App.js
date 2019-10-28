b33a58d66082ae40d6d48c81b7faa6ccb85e1b5c
aris winandi <winandiaris@gmail.com>
2019-10-28 14:00:25 +0700

    menambahkan komponen ShopPage, CollectionPreview, dan CollectionItem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 //import logo from './logo.svg';
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>hat page</h1>
-  </div>
-)
 
 function App() {
   return (
@@ -16,8 +12,7 @@ function App() {
       {/* <HomePage></HomePage> */}
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
       
     </div>
