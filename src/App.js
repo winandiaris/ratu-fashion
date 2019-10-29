ce007bba8a01eddfe57b62b1c80c109927bd2e0b
aris winandi <winandiaris@gmail.com>
2019-10-29 13:14:42 +0700

    membuat form signin dan custom button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
+import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignOut}/>
       </Switch>
       
     </div>
